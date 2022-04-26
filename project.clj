b0bada85a35d10523fed64917f00181f9be5618a
James Griffiths <64775694+MIJOTHY-V2@users.noreply.github.com>
2022-04-26 14:02:50 +0100

    feat: add test location metadata option (#1)
    Certain tools support making use of file/line/col metadata from junit
    xml files. This commit adds an option for adding this metadata to the
    junit output.
    
    Relates to
    https://github.com/lambdaisland/kaocha-junit-xml/pull/13

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject droit.tech/kaocha-junit-xml "0.0.78"
+  :description "Kaocha plugin to generate a JUnit XML version of the test results."
+  :url "https://github.com/droitfintech/kaocha-junit-xml"
+  :dependencies
+  [[org.clojure/clojure "1.10.0"]
+   [lambdaisland/kaocha "1.65.1029"]]
+  :profiles
+  {:test
+   {:dependencies
+    [[nubank/matcher-combinators    "0.6.1"]
+     [org.clojure/data.xml          "0.0.8"]
+     [lambdaisland/kaocha-cloverage "0.0-22"]]}}
+
+  :aliases
+  {"kaocha" ["run" "-m" "kaocha.runner"]
+   "test" ["with-profiles" "+test" "kaocha"]})
